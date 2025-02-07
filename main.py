89334c2536e5ca6bd329132ae7d2b73b9668d3eb
ktmrose <ktm.rose@yahoo.com>
2025-02-07 16:34:58 -0700

    room code generated on init

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from websockets.asyncio.server import serve
 import asyncio
+from string import ascii_uppercase
+from random import choice
 
 PORT = 7890
 
-print("Server listening on port " + str(PORT))
+def generate_room_code(length):
+    room_code = ''
+    for i in range(length):
+        room_code = room_code + choice(ascii_uppercase)
+    return room_code
+    
 
 async def echo(websocket):
     print("A client connected")
@@ -13,6 +20,7 @@ async def echo(websocket):
 
 async def main():
     async with serve(echo, "localhost", PORT) as server:
+        print("Session started. Room code: " + generate_room_code(4))
         await server.serve_forever()
 
 
